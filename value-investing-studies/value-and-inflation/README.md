05d3e5de633bec534dc78540bdd3b522b7bdea54
Swarnajyoti Mukherjee <41522093+swarnajyoti@users.noreply.github.com>
2022-01-19 16:23:29 +0100

    changed

---
+++ value-investing-studies/value-and-inflation/README.md
@@ -0,0 +1,63 @@
+# The Relationship Between Value Investing and Inflation
+
+![alt text](/value-and-inflation/value-cpi-decade-chart.png)
+
+For details on this chart and the data used to generate it, please [visit this post](https://www.euclidean.com/value-investing-as-a-hedge-against-inflation).
+
+## Installing and Executing the Code
+
+###  Requirements
+
+You will need the programs git and Rscript installed on your computer. The examples below are for Mac OS X or any Linux type OS.
+
+### Cloning the Git Repository
+
+To clone the repository, make sure you’re in a directory on your computer that you want to hold the files. Use the command 
+
+```bash
+$ git clone https://github.com/euclidjda/value-investing-studies.git
+```
+
+to copy the repository “value-investing-studies” into your current directory. 
+
+### Installing Necessary R Dependecies 
+
+Some R dependecies are required to run our scripts. Use the command: 
+
+```bash 
+$ cd value-investing-studies
+``` 
+
+to navigate into your newly cloned directory. To install the necessary dependencies, use the command: 
+
+```bash 
+$ Rscript install_dependecies.R
+``` 
+
+After the script completes, you will have all the R dependecies required to run any scripts within the value-investing-studies repo. 
+
+### Executing the Code
+
+Navigate to the directory where the script resides by typing 
+
+```bash
+$ cd value-and-inflation
+```
+
+and execute the script with the command 
+
+```bash 
+$ Rscript value-cpi-decade-chart.R
+```
+
+## Contributors and Acknowledgement
+
+This repository was developed and is maintained by [Euclidean Technologies, LLC](http://www.euclidean.com/). We would like to thank [Kenneth R. French](http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/) and [Robert Shiller](http://www.econ.yale.edu/~shiller/) for making the stock market and economic data used by this repo available through their websites [here](http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html#Research) and [here](http://www.econ.yale.edu/~shiller/data.htm), respectively. The equity investment return data is copyrighted by Kenneth R. French.
+
+In addition, some of the studies here were not originally conceived by [Euclidean](http://www.euclidean.com/). In particular, William Bernstein first [published](http://www.efficientfrontier.com/ef/701/value.htm) a version of the value-and-inflation study in 2001. Star Capital [published](http://www.starcapital.de/files/publikationen/Research_2016-09_Value_Premium_Fama_French.pdf) a version of the value-vs-growth analysis in 2016.
+
+## License 
+
+This is experimental software. It is provided under the [MIT license][mit], so you can do with it whatever you wish except hold the authors responsible if it does something you don't like.
+
+[mit]: http://www.opensource.org/licenses/mit-license.php
---
+++ value-investing-studies/value-and-inflation/value-cpi-decade-chart.R
@@ -0,0 +1,173 @@
+#!/usr/bin/env Rscript
+
+# Load libraries
+library(ggplot2)
+library(stringr)
+library(scales)
+library(ggthemes)
+library(PerformanceAnalytics)
+library(RColorBrewer)
+library(plyr)
+library(gdata)
+
+# Define geometric mean
+geomean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
+# Download Ken French's value performance data
+url.name     <- "http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp"
+file.name    <- "F-F_Research_Data_Factors_TXT.zip"
+full.url     <- paste(url.name, file.name, sep="/")
+
+# You can change the ending date of the analysis by changing these parameters
+end.year     <- 2019 
+end.month    <- 12
+
+if (!file.exists(file.name)) {
+    # Download the data and unzip it
+    download.file(full.url, file.name)
+}
+
+file.list <- unzip(file.name, list=TRUE)
+
+# Parse the data
+french.data   <- read.csv(unzip(file.name,files=as.character(file.list[1,1])),
+                          sep = "",
+                          skip = 4,
+                          header=FALSE )
+
+# Set the column headers
+names(french.data) <- list("DATE","Mkt-RF","SMB","HML","RF")
+
+# Now we want to remove all the data below the end date
+ds.year     <- as.numeric(substr(french.data$DATE[[1]],1,4))
+ds.month    <- as.numeric(substr(french.data$DATE[[1]],5,6))
+num.rows    <- 12 * (end.year-ds.year)+(end.month-ds.month)+1
+french.data <- head(french.data,num.rows)
+date.seq    <- as.Date(paste(french.data$DATE,"01",sep=""),"%Y%m%d")
+french.data$DATE <- date.seq
+
+# Transform the data so that the return cells are numeric decimal format
+for (i in 2:ncol(french.data)) french.data[,i] <- as.numeric(str_trim(french.data[,i]))
+for (i in 2:ncol(french.data)) french.data[,i] <- french.data[,i]/100
+
+french.data$Hi.Lo <- french.data$HML
+
+# Converting monthly value data into annual value data
+ts.data <- data.frame(french.data$Hi.Lo)
+row.names(ts.data) <- date.seq
+hi.lo.monthly <- xts(ts.data$french.data.Hi.Lo,date.seq)
+hi.lo.annual  <- aggregate(hi.lo.monthly+1,
+                           as.integer(format(index(hi.lo.monthly),"%Y")), prod)-1
+
+# Downloading Shiller spreadsheet for inflation data
+
+# The URL for the data
+shiller.data.url <- 'http://www.econ.yale.edu/~shiller/data/ie_data.xls'
+
+# Name of file to store data on local computer 
+shiller.filename <- 'shillerdata.xls'
+
+if (!file.exists(shiller.filename)) {
+    # Download the data 
+    download.file(shiller.data.url, shiller.filename, mode='wb')
+
+}
+
+# Parse the data 
+shiller.data <- read.xls(shiller.filename, sheet='Data', skip=6, header=TRUE)
+
+# Extract data needed
+shiller.data <- shiller.data[, c("Date", "CPI")]
+shiller.data <- head(shiller.data, nrow(shiller.data) - 2) ## Last row does not contain relevant data for inflation
+shiller.data$CPI <- as.numeric(as.character(shiller.data$CPI))
+
+# Will need to be further trimmed to align with timeline of French data 
+
+# Turn shiller.data into annualized data #
+
+# Selecting only the dates needed from Shiller data. French.data
+# has less data points than shiller.data so we must choose proper
+# dates to align the two data sets
+shiller.data$Date <- str_replace_all(shiller.data$Date, '[[.]]', '')
+shiller.data$Date <- as.Date(paste(shiller.data$Date, '01', sep=''), '%Y%m%d')
+start.index <- which(shiller.data$Date == as.Date('1926-07-01'))[1]
+end.index <- which(shiller.data$Date == as.Date(paste(end.year,'12-01',sep='-')))[1]
+shiller.data <- shiller.data[start.index : end.index, ]
+
+# Converting monthly inflation data into annual inflation data #
+cpi.data       <- xts(shiller.data$CPI, shiller.data$Date)
+cpi.monthly    <- Return.calculate(cpi.data, method="compound")
+cpi.monthly    <- cpi.monthly[2:nrow(cpi.monthly),]
+cpi.annual     <- aggregate(cpi.monthly+1,
+                            as.integer(format(index(cpi.monthly),"%Y")), prod)-1
+
+# Create new data frame as zoo object
+data.annual    <- merge.zoo( cpi.annual, hi.lo.annual )
+df.annual      <- data.frame(cpi=data.annual$cpi.annual,hilo=data.annual$hi.lo.annual)
+
+df.annual$year <- rownames(df.annual)
+
+# Computing the data for the decade
+cpi.decade     <- aggregate(cpi.annual+1,
+                            as.integer(substr(index(cpi.annual),1,3)), geomean)-1
+hi.lo.decade   <- aggregate(hi.lo.annual+1,
+                            as.integer(substr(index(hi.lo.annual),1,3)), geomean)-1
+data.decade    <- merge.zoo( cpi.decade, hi.lo.decade )
+
+df.decade      <- data.frame(cpi=data.decade$cpi.decade,hilo=data.decade$hi.lo.decade)
+df.decade$year <- rownames(df.decade)
+
+lm_eqn = function(m) {
+
+  l <- list(a = format(coef(m)[1], digits = 2),
+      b = format(abs(coef(m)[2]), digits = 2),
+      r2 = format(summary(m)$r.squared, digits = 3));
+
+  if (coef(m)[2] >= 0)  {
+    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
+  } else {
+    eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(r)^2~"="~r2,l)    
+  }
+
+  as.character(as.expression(eq));                 
+}
+
+# Creating the png canvas to draw graph on
+png.filename <- "value-cpi-decade-chart.png"
+
+png(png.filename, width=1024, height=640)
+# png(png.filename, width=768, height=480)
+
+p <- ggplot(df.decade,aes(x=cpi,y=hilo))
+p <- p + geom_smooth(method="lm",se=FALSE,linetype = "dotted")
+p <- p + geom_point(color="#DD592D",size=3)
+
+# To see equation for best fit, uncomment the following two lines
+# p <- p + geom_text(aes(x = 0.05, y = -0.01, label = lm_eqn(lm(hilo ~ cpi, df.decade))),
+#                   parse = TRUE,size=4,color='#617994')
+
+p <- p + geom_text(data=subset(df.decade, (year<195.5 | year==197) & year!=193),
+                   aes(label=paste(year,"0s",sep=""),
+                       vjust=-.8,hjust=0.8),size=4,color='#617994')
+p <- p + geom_text(data=subset(df.decade, (year>195.5 | year==193) & year!=197),
+                   aes(label=paste(year,"0s",sep=""),vjust=-.8,hjust=0.1),
+                   size=4,color='#617994')
+
+p <- p + ylim(-0.1,0.2)
+
+p <- p + scale_x_continuous("Annualized Rate of Inflation",
+                            label=percent,
+                            c(-0.04,-0.02,0,0.02,0.04,0.06,0.08))
+p <- p + scale_y_continuous("Compound Annualized Value Factor",
+                            label=percent,
+                            limits=c(-0.04,0.12),breaks=c(-0.04,-0.02,0,0.02,0.04,0.06,0.08,0.10,0.12))+ggtitle("\n\n\n\n")
+p <- p + ggtitle("The Relationship Between Inflation and Value Investing By Decade")
+p <- p + theme(plot.title = element_text(hjust = 0.5))
+p <- p + theme_minimal()
+
+p
+dev.off()
+# Opening the png file 
+system2('open', args = png.filename, wait = FALSE)
